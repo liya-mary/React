1ea73b57e88a88ff2cfc0605f90e79065c171d0d
liya-mary <maryliya007@gmail.com>
2025-08-08 15:03:35 +0200

    episode-3

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    [React.createElement("div", { id: "child", key: 1 },
-        [React.createElement("h1", { key: 5 }, "I am h1  tag"), React.createElement("h2", { key: 3 }, "I am h2  tag")]
-    ),
-    React.createElement("div", { id: "child", key: 2 },
-        [React.createElement("h1", { key: 7 }, "I am h1  tag"), React.createElement("h2", { key: 4 }, "I am h2  tag")]
-    )]
-)
+//jsx
+//Babel transpile jsx to react...
+const jsxHeading = <h1>Namste jsx react</h1>
 
 
+const number = 1000;
+const HeadingComponent = () => {
+    return (
+        <div>
+            {number}
+            <h1 className="heading">Namaste React functional component</h1>
+        </div>
 
-const head = React.createElement("h1", {}, "Hello from react....");
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(head);
-
-console.log("parent: ", parent);
-root.render(parent);
-
+root.render(<HeadingComponent />)
